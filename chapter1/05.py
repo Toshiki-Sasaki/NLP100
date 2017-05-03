662e36e4752176a0a5745f4d88f3e70d191863f8
Toshiki SASAKI <blackraccoon123@gmail.com>
2017-05-03 17:18:01 +0000

    6番追加

--- chapter1/05.py
+++ chapter1/05.py
@@ -1,22 +1,27 @@
 # -*- coding:utf-8 -*-
 import re
 
-def ngram(sentence, n):
-    alpha = re.split('\W+', sentence)
+def word_ngram(sentence, n):
     # word n-gram
+    alpha = re.split('\W+', sentence)
     wordN = [alpha[i:i+n] for i in range(len(alpha)-(n-1))]
+    print(wordN)
+
+def alpha_ngram(sentence, n):
     # alphabet n-gram
+    alpha = re.split('\W+', sentence)
     alphaN = [''.join(alpha)[i:i+n] for i in range(len(''.join(alpha))-(n-1))]
-    return wordN, alphaN
+    print(alphaN)
 
 sentence = 'I am an NLPer'
 n=2
 
-wordN, alphaN = ngram(sentence, n)
-print(wordN)
-print(alphaN)
+word_ngram(sentence, n)
+alpha_ngram(sentence, n)
 
 '''
 >>> [['I', 'am'], ['am', 'an'], ['an', 'NLPer']]
 >>> ['Ia', 'am', 'ma', 'an', 'nN', 'NL', 'LP', 'Pe', 'er']
 '''
+
+# python には ngram というライブラリがあるので，それを使えば楽．
---
+++ chapter1/06.py
@@ -0,0 +1,41 @@
+#-*- coding:utf-8 -*-
+
+def bi_gram(sen):
+    arr = []
+    sen = list(sen)
+    for i in range(len(sen)-1):
+        arr.append("".join(sen[i:i+2]))
+    return arr
+
+def intersection(X, Y):
+    arr = []
+    for i in range(len(X)):
+        if not X[i] in arr:
+            if X[i] in Y:
+                arr.append(X[i])
+    return arr
+    
+def union(X, Y):
+    arr = []
+    for i in range(len(X)):
+        if not X[i] in arr:
+            arr.append(X[i])
+    for j in range(len(Y)):
+        if not Y[j] in arr:
+            arr.append(Y[j])
+    return arr
+    
+sentence1 = "paraparaparadise"
+sentence2 = "paragraph"
+
+X = bi_gram(sentence1)
+Y = bi_gram(sentence2)
+
+U = union(X, Y)
+I = intersection(X, Y)
+
+print(U)
+print(I)
+
+#print(set(X) | set(Y))
+#print(set(X) & set(Y))
