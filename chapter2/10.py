d0138fb6f2c11f6e18727b0c3b3e01552fdde7e5
Toshiki SASAKI <blackraccoon123@gmail.com>
2017-05-04 14:01:59 +0000

    10番追加

---
+++ chapter2/10.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def _get_txtlength_mine(txt):
+    f = open(txt)
+    return len(f.readlines())
+
+txt = 'hightemp.txt'
+print(_get_txtlength_mine(txt))
+
+# wc -l hightemp.txt
