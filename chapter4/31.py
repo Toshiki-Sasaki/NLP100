1290eced043590db57399e18d66637d7ede6c8f2
toshiki sasaki <toshiki-s@h28is012.naist.jp>
2017-06-06 00:05:07 +0900

    31番追加

--- chapter4/31.py
+++ chapter4/31.py
@@ -1,2 +1,23 @@
 #!usr/bin/env python3
 # -*- coding:utf-8 -*-
+
+import p30
+import json
+
+def extractVerbs(INPUT):
+    D = []
+    j = 0
+    for morpheme in INPUT:
+        for i in morpheme:
+            if i['pos'] == '動詞':
+                D.append(i['surface'])
+                j += 1
+    return D
+
+if __name__ == '__main__':
+    INPUT = 'neko.txt.mecab'
+    OUTPUT = 'neko_31.json'
+    d = p30.getMorphemeDict(INPUT)
+    D = extractVerbs(d)
+    #o = open(OUTPUT, 'w')
+    #json.dump(D, o, ensure_ascii=False)
--- chapter4/p30.py
+++ chapter4/p30.py
@@ -2,31 +2,28 @@
 # -*- coding:utf-8 -*-
 
 import codecs
-import json
 
 def getMorphemeDict(INPUT):
     f = open(INPUT, 'r')
-    o = open(OUTPUT, 'w')
     tmpArray = []
     dictArray = []
     for line in f:
         data = line.rstrip().split('\t')
+        # EOF(End of file)以外を実行
         try:
             feature = data[1].split(',')
         except IndexError:
             print("EOS")
         morphemeDict = {'surface':data[0], 'base':feature[6], 'pos':feature[0], 'pos1':feature[1]}
         tmpArray.append(morphemeDict)
+        # 文の最後であれば、文をリストとして表現。
         if feature[1] == '句点':
             dictArray.append(tmpArray)
             tmpArray = []
-    json.dump(dictArray, o, ensure_ascii=False)
     f.close()
-    o.close()
     return dictArray
 
 if __name__ == '__main__':
     INPUT = 'neko.txt.mecab'
-    OUTPUT = 'neko_30.json'
     #OUTPUT = 'neko_30.txt'
     d = getMorphemeDict(INPUT)
