63108b30ac3f2ece116904c8c5b6866537eb8066
toshiki sasaki <blackraccoon123@gmail.com>
2017-06-06 20:37:52 +0900

    ファイル修正

--- chapter4/33.py
+++ chapter4/33.py
@@ -4,7 +4,7 @@
 import p30
 import json
 
-def extractAofB(INPUT):
+def extractVerbalConnection(INPUT):
     D = []
     j=0
     for morpheme in INPUT:
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     INPUT = 'neko.txt.mecab'
     OUTPUT = 'results/neko_33.json'
     d = p30.getMorphemeDict(INPUT)
-    D = extractAofB(d)
+    D = extractVerbalConnection(d)
     #print(len(D))
     #print(D[0:10])
     o = open(OUTPUT, 'w')
--- chapter4/35.py
+++ chapter4/35.py
@@ -4,24 +4,19 @@
 import p30
 import json
 
-def extractVerbs(INPUT):
-    d = []
-    # naiveな方法なので無駄があるコード
+def extractNounJunc(INPUT):
+    w = []
     for morpheme in INPUT:
-        for i in range(len(morpheme)-2):
-            data =  morpheme[i:i+3]
-            m1 = morpheme[i]['pos'] == '名詞'
-            m2 = morpheme[i+1]['surface'] == 'の'
-            m3 = morpheme[i+2]['pos'] == '名詞'
-            if m1 and m2 and m3:
-                d.append(''.join([i['surface'] for i in data]))
+        for i in morpheme:
+            if i['surface'] == '名詞':
+                w.append(i['surface'])
     return d
 
 if __name__ == '__main__':
     INPUT = 'neko.txt.mecab'
     OUTPUT = 'results/neko_35.json'
     d = p30.getMorphemeDict(INPUT)
-    D = extractVerbs(d)
+    D = extractNounJunc(d)
     print(len(D))
     print(D[0:10])
     o = open(OUTPUT, 'w')
