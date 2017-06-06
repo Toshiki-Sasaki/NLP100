2358ee51948723a0bbe40e169eb53be7dfa5c3f1
toshiki sasaki <toshiki-s@h28is012.naist.jp>
2017-06-06 11:47:01 +0900

    34番追加

---
+++ chapter4/34.py
@@ -0,0 +1,28 @@
+#!usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import p30
+import json
+
+def extractVerbs(INPUT):
+    d = []
+    # naiveな方法なので無駄があるコード
+    for morpheme in INPUT:
+        for i in range(len(morpheme)-2):
+            data =  morpheme[i:i+3]
+            m1 = morpheme[i]['pos'] == '名詞'
+            m2 = morpheme[i+1]['surface'] == 'の'
+            m3 = morpheme[i+2]['pos'] == '名詞'
+            if m1 and m2 and m3:
+                d.append(''.join([i['surface'] for i in data]))
+    return d
+
+if __name__ == '__main__':
+    INPUT = 'neko.txt.mecab'
+    OUTPUT = 'results/neko_34.json'
+    d = p30.getMorphemeDict(INPUT)
+    D = extractVerbs(d)
+    print(len(D))
+    print(D[0:10])
+    o = open(OUTPUT, 'w')
+    json.dump(D, o, ensure_ascii=False)
