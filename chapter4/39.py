c1a3ab73b15a8bc74cd6b067451e729d5d4d1643
toshiki sasaki <blackraccoon123@gmail.com>
2017-06-06 22:34:09 +0900

    39番追加

---
+++ chapter4/39.py
@@ -0,0 +1,42 @@
+#!usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import p30
+import json
+from collections import defaultdict #出現頻度は求められるが、ソートする必要がある
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import seaborn as sns
+
+def getFrequency(INPUT):
+    morphemeDict = defaultdict(int)
+    m = []
+    for morpheme in INPUT:
+        for i in morpheme:
+            morphemeDict[i['surface']] += 1
+    for k, v in sorted(morphemeDict.items(), key=lambda x: x[1], reverse=True):
+        m.append(", ".join([k, str(v)]))
+    return m
+
+def zipf(D):
+    freq = []
+    for i in D:
+        d = i.split(',')
+        freq.append(int(d[1]))
+    rank = np.arange(1,len(freq)+1)
+    mpl.rcParams['font.family'] = 'AppleGothic'
+    plt.xlabel('出現頻度順位')
+    plt.ylabel('出現頻度')
+    plt.xscale('log')
+    plt.yscale('log')
+    sns.set_palette('bright')
+    plt.scatter(rank, freq, s=15)
+    plt.show()
+
+if __name__ == '__main__':
+    INPUT = 'neko.txt.mecab'
+    OUTPUT = 'results/neko_36.json'
+    d = p30.getMorphemeDict(INPUT)
+    D = getFrequency(d)
+    zipf(D)
