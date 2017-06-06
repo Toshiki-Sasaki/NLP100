0b9210f8cfa586c71e953dfd92879d9bc6ef21b9
toshiki sasaki <blackraccoon123@gmail.com>
2017-06-06 21:53:54 +0900

    37番追加

--- chapter4/37.py
+++ chapter4/37.py
@@ -4,8 +4,10 @@
 import p30
 import json
 from collections import defaultdict #出現頻度は求められるが、ソートする必要がある
-#from collections import Counter #出現頻度の多い順とかにするならやっぱCounterが便利そう?
-#collectionsに関しては19番参考
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import seaborn
 
 def getFrequency(INPUT):
     morphemeDict = defaultdict(int)
@@ -17,12 +19,24 @@ def getFrequency(INPUT):
         m.append(", ".join([k, str(v)]))
     return m
 
+def genBar(D):
+    label = []
+    freq = []
+    for i in D:
+        d = i.split(',')
+        label.append(d[0])
+        freq.append(int(d[1]))
+    x = np.arange(0,len(freq))
+    mpl.rcParams['font.family'] = 'AppleGothic'
+    plt.bar(x, freq, tick_label=label, align="center")
+    plt.tick_params(labelsize=15)
+    plt.show()
+
 if __name__ == '__main__':
     INPUT = 'neko.txt.mecab'
     OUTPUT = 'results/neko_36.json'
     d = p30.getMorphemeDict(INPUT)
     D = getFrequency(d)
-    print(len(D))
-    print(D[0:10])
+    genBar(D[0:10])
     o = open(OUTPUT, 'w')
     json.dump(D, o, ensure_ascii=False)
